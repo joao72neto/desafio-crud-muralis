1b85628331f31a2fcbaca1600aa2c512441502c4
joao_s_neto <joao72neto@gmail.com>
2025-03-30 13:34:58 -0300

    feat: adicionar estrutura base do backend

---
+++ backend/src/main/java/com/muralis/app/CrudMuralisApplication.java
@@ -0,0 +1,13 @@
+package com.muralis.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMuralisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMuralisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/muralis/app/CrudMuralisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muralis.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMuralisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
