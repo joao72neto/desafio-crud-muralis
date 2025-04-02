1848412255396573932c680ab444d716542dfe1f
joao_s_neto <joao72neto@gmail.com>
2025-04-01 22:12:19 -0300

    feat: carregar os clientes do banco de dados

--- frontend/javaScript/service/clientes.js
+++ frontend/javaScript/service/clientes.js
@@ -4,31 +4,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
 async function fetchClientes() {
     try {
-        const response = await fetch('http://localhost:3000/clientes'); 
+        const response = await fetch('http://localhost:8080/clientes'); 
         const clientes = await response.json();
 
-        const wrapper = document.querySelector('.wrapper'); 
-        wrapper.innerHTML = ''; 
+        //Obtendo o container
+        const container = document.querySelector('.container-index')
 
         clientes.forEach(cliente => {
-            const divCliente = document.createElement('div');
-            divCliente.classList.add('cliente');
-            divCliente.innerHTML = `
-                <p class="invisible cliente-id">${cliente.id}</p>
-                <p>${cliente.nome}</p>
-                <p>${cliente.email}</p>
+            const clienteDiv = document.createElement('div');
+
+            clienteDiv.classList.add('wrapper');
+            clienteDiv.innerHTML = `
+                <div class="cliente">
+                    <p class="invisible cliente-id">1</p>
+                    <p>${cliente.clt_nome}</p>
+                    <p>${cliente.clt_data_nasc}</p>
+                </div>
+                <div class="acoes">
+                    <a class="cont">Contatos</a>
+                    <a class="edit">Editar</a>
+                    <a class="excl">Excluir</a>
+                </div>
             `;
 
-            const divAcoes = document.createElement('div');
-            divAcoes.classList.add('acoes');
-            divAcoes.innerHTML = `
-                <a class="cont">Contatos</a>
-                <a class="edit">Editar</a>
-                <a class="excl">Excluir</a>
-            `;
-
-            wrapper.appendChild(divCliente);
-            wrapper.appendChild(divAcoes);
+            container.appendChild(clienteDiv);
         });
     } catch (error) {
         console.error('Erro ao buscar clientes:', error);
